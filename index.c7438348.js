0f8ac2a3c506525f55d81462a1ae580613f0f0a5
g0d2illa <115025828+g0d2illa@users.noreply.github.com>
2023-03-28 18:21:58 +0000

    Deploying to gh-pages from @ g0d2illa/goit-js-hw-10@39bfce97f885a016808acf877b264ab8039b7ef8 🚀

--- index.c7438348.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");fetch("https://restcountries.com/v3.1/all").then((o=>{if(console.log(o),!o.ok)throw new Error(o.status);return o.json()})).then((o=>console.log(o))).catch((o=>console.log(o)))}();
-//# sourceMappingURL=index.c7438348.js.map
