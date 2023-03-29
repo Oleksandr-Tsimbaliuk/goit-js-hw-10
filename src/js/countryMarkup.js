29a03265c9b4d1d3580bb1d864dd3fee582d2a02
Oleksandr <115025828+g0d2illa@users.noreply.github.com>
2023-03-29 13:09:05 +0300

    Update countryMarkup.js

--- src/js/countryMarkup.js
+++ src/js/countryMarkup.js
@@ -20,17 +20,23 @@ function createCountryInfo(arrayCountries) {
   firstCountry = arrayCountries[0];
   return `
   <div class="country-info-box">
-      <img class="country-img"
-        src="${firstCountry.flags.svg}"
-        alt="country-img" width="30" height="20" />
-      <h2 class="country-name">${firstCountry.name.common}</h2>
-    </div>
-      <ul class="country-info-list">
-        <li class="info-list-item">Capital: ${firstCountry.capital}</li>
-        <li class="info-list-item">Population: ${firstCountry.population}</li>
-        <li class="info-list-item">Languages: ${Object.values(
-          firstCountry.languages
-        ).join(', ')}</li></div>`;
+  <img
+    class="country-img"
+    src="${firstCountry.flags.svg}"
+    alt="country-img"
+    width="30"
+    height="20"
+  />
+  <h2 class="country-name">${firstCountry.name.common}</h2>
+</div>
+
+<ul class="country-info-list">
+  <li class="info-list-item">Capital: ${firstCountry.capital}</li>
+  <li class="info-list-item">Population: ${firstCountry.population}</li>
+  <li class="info-list-item">
+    Languages: ${Object.values(firstCountry.languages).join(', ')}
+  </li>
+</ul>`;
 }
 
 export { createCountryList, createCountryInfo };
